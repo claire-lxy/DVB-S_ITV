c8c091d0e8f73735296bbc8701f2e1622386beff
weiwenhuan <weiwenhuan@konka.com>
2019-07-23 17:38:35 +0800

    开启自启动暂时屏蔽启动BookService，防止和launcher抢占消息回调和surface

--- app/src/main/java/com/konkawise/dtv/receiver/BookCompletedReceiver.java
+++ app/src/main/java/com/konkawise/dtv/receiver/BookCompletedReceiver.java
@@ -4,13 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.konkawise.dtv.service.BookService;
-
 public class BookCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            BookService.bootService(new Intent(context, BookService.class));
+//            BookService.bootService(new Intent(context, BookService.class));
         }
     }
 }
